74358a69fb35ed53e473263cb366badc9f098e79
Rachel Spencer <rachelspencer688@gmail.com>
2022-12-30 14:45:25 -0500

    with O & X rendering and resetting board functionality

--- src/App.js
+++ src/App.js
@@ -7,16 +7,30 @@ function App() {
   const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
   const [player, setPlayer] = useState(true);
 
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', '']);
+    setPlayer(true);
+  }
+
+
   return (
     <div className="App">
-      <Square 
-      squares={squares} 
-      setSquares={setSquares}
-      player={player} 
-      setPlayer={setPlayer}
-      />
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares} 
+              player={player} 
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
-}
-
+};
 export default App;
