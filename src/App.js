9f5636e02e49c9df583437bb454627561f20605b
Rachel Spencer <rachelspencer688@gmail.com>
2022-12-30 15:15:51 -0500

    final with calculate winner function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,27 @@ function App() {
     setSquares(['', '', '', '', '', '', '', '', '']);
     setPlayer(true);
   }
+ const calculateWinner = (arr) => {
+  const lines = [
+    [0, 4, 8],
+    [0, 1, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
 
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+
+    if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+      return `${arr[a]} won!`
+    } 
+  }
+  return "Who will win?";
+ };
 
   return (
     <div className="App">
@@ -30,6 +50,7 @@ function App() {
         })}
       </div>
       <button onClick={handleClick}>Reset</button>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 };
